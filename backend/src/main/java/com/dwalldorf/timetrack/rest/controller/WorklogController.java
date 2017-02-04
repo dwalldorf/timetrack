2029823a62904ba8982abe12b4a79bd690c4e74e
Daniel Walldorf <d.walldorf@me.com>
2017-02-04 23:08:37 +0100

    WorklogControllerIT

--- backend/src/main/java/com/dwalldorf/timetrack/rest/controller/WorklogController.java
+++ backend/src/main/java/com/dwalldorf/timetrack/rest/controller/WorklogController.java
@@ -1,14 +1,21 @@
 package com.dwalldorf.timetrack.rest.controller;
 
 import com.dwalldorf.timetrack.document.WorklogEntry;
+import com.dwalldorf.timetrack.rest.dto.ListDto;
+import com.dwalldorf.timetrack.rest.dto.WorklogEntryDto;
 import com.dwalldorf.timetrack.service.WorklogService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(WorklogController.BASE_URI)
 public class WorklogController {
+
+    public static final String BASE_URI = "/worklog";
 
     private final WorklogService worklogService;
 
@@ -17,8 +24,18 @@ public class WorklogController {
         this.worklogService = worklogService;
     }
 
-    @GetMapping("/worklog")
-    public List<WorklogEntry> getWorklog() {
-        return worklogService.findAll();
+    @GetMapping
+    public ListDto<WorklogEntryDto> getWorklog() {
+        List<WorklogEntry> worklogEntries = worklogService.findAll();
+        return toDtoList(worklogEntries);
+    }
+
+    private ListDto<WorklogEntryDto> toDtoList(List<WorklogEntry> documents) {
+        ArrayList<WorklogEntryDto> dtos = new ArrayList<>();
+        documents.forEach(document -> {
+            dtos.add(new WorklogEntryDto(document));
+        });
+
+        return new ListDto<>(dtos);
     }
 }
