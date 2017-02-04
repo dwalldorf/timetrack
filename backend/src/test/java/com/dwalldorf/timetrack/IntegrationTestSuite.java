65f6e178e67eb90c509f6da989a30e9b64e29ee0
Daniel Walldorf <d.walldorf@me.com>
2017-02-04 18:40:11 +0100

    wip

--- backend/src/test/java/com/dwalldorf/timetrack/IntegrationTestSuite.java
+++ backend/src/test/java/com/dwalldorf/timetrack/IntegrationTestSuite.java
@@ -1,12 +1,16 @@
 package com.dwalldorf.timetrack;
 
+import com.dwalldorf.timetrack.rest.controller.CsvUploadControllerIT;
 import com.dwalldorf.timetrack.rest.controller.VersionControllerIT;
+import com.dwalldorf.timetrack.rest.controller.WorklogControllerIT;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        VersionControllerIT.class
+        CsvUploadControllerIT.class,
+        VersionControllerIT.class,
+        WorklogControllerIT.class
 })
 public class IntegrationTestSuite {
 }
---
+++ backend/src/test/java/com/dwalldorf/timetrack/rest/controller/WorklogControllerIT.java
@@ -0,0 +1,11 @@
+package com.dwalldorf.timetrack.rest.controller;
+
+import org.junit.Test;
+
+public class WorklogControllerIT extends BaseControllerIT {
+
+    @Test
+    public void testGetWorklog() throws Exception {
+        // TODO: implement tests
+    }
+}
