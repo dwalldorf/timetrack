1f80a82bbb6e4c772b7aa670ff613b7c58a4d709
Daniel Walldorf <d.walldorf@me.com>
2017-02-10 18:04:36 +0100

    remove unused exception constructors

--- backend/src/main/java/com/dwalldorf/timetrack/backend/exception/LoginRequiredException.java
+++ backend/src/main/java/com/dwalldorf/timetrack/backend/exception/LoginRequiredException.java
@@ -2,22 +2,7 @@ package com.dwalldorf.timetrack.backend.exception;
 
 public class LoginRequiredException extends RuntimeException {
 
-    public LoginRequiredException() {
-    }
-
     public LoginRequiredException(String message) {
         super(message);
-    }
-
-    public LoginRequiredException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LoginRequiredException(Throwable cause) {
-        super(cause);
-    }
-
-    public LoginRequiredException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
