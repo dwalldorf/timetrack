03f98b99478938dd2af415ec25330eefaaca2c67
Daniel Walldorf <d.walldorf@me.com>
2017-02-11 11:11:49 +0100

    WorklogEntryDaoTest wip

--- backend-repository/src/test/java/com/dwalldorf/timetrack/repository/dao/WorklogEntryDaoTest.java
+++ backend-repository/src/test/java/com/dwalldorf/timetrack/repository/dao/WorklogEntryDaoTest.java
@@ -1,11 +1,30 @@
 package com.dwalldorf.timetrack.repository.dao;
 
+import static org.mockito.Mockito.*;
+
+import com.dwalldorf.timetrack.repository.repository.WorklogRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WorklogEntryDaoTest {
+
+    private WorklogRepository worklogRepository;
+
+    private WorklogEntryDao worklogEntryDao;
+
+    @Before
+    public void setUp() throws Exception {
+        this.worklogRepository = mock(WorklogRepository.class);
+        this.worklogEntryDao = new WorklogEntryDao(worklogRepository);
+    }
+
+    @Test
+    public void toDocument_WithNull() {
+
+    }
 
     @Test
     public void toDocument() {
@@ -14,6 +33,11 @@ public class WorklogEntryDaoTest {
 
     @Test
     public void toDocumentList() {
+
+    }
+
+    @Test
+    public void toModel_WithNull() {
 
     }
 
