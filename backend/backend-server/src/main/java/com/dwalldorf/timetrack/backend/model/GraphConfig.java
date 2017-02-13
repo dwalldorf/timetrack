2c392a3e522f516d5c65a855b373b38a4db5038b
Daniel Walldorf <d.walldorf@me.com>
2017-02-13 21:45:11 +0100

    add scale

--- backend/backend-server/src/main/java/com/dwalldorf/timetrack/backend/model/GraphConfig.java
+++ backend/backend-server/src/main/java/com/dwalldorf/timetrack/backend/model/GraphConfig.java
@@ -5,19 +5,22 @@ import org.joda.time.DateTime;
 
 public class GraphConfig implements Serializable {
 
+    public enum Scale {
+        DAY,
+        WEEK,
+        MONTH
+    }
+
     private DateTime from;
 
     private DateTime to;
+
+    private Scale scale;
 
     public DateTime getFrom() {
         return from;
     }
 
-    /**
-     *
-     * @param from
-     * @return
-     */
     public GraphConfig setFrom(DateTime from) {
         this.from = from;
         return this;
@@ -29,6 +32,15 @@ public class GraphConfig implements Serializable {
 
     public GraphConfig setTo(DateTime to) {
         this.to = to;
+        return this;
+    }
+
+    public Scale getScale() {
+        return scale;
+    }
+
+    public GraphConfig setScale(Scale scale) {
+        this.scale = scale;
         return this;
     }
 }
