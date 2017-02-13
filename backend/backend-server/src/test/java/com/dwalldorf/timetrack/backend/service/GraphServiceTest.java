589c7b456d5fcd9fdd5d27c35befceb2bd9f3c25
Daniel Walldorf <d.walldorf@me.com>
2017-02-13 21:45:46 +0100

    test parsing of scale string

--- backend/backend-server/src/test/java/com/dwalldorf/timetrack/backend/service/GraphServiceTest.java
+++ backend/backend-server/src/test/java/com/dwalldorf/timetrack/backend/service/GraphServiceTest.java
@@ -16,13 +16,14 @@ public class GraphServiceTest extends BaseTest {
     private static final String STR_DATE_TO = "2017-01-01";
     private static final String STR_INVALID_DATE_1 = "invalid date.";
     private static final String STR_INVALID_DATE_2 = "2017-01-01T12:15:45.000+01:00";
-    private static final String STR_EMPTY = "";
     private static final String STR_TODAY = "today";
 
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static final String STR_SCALE_DAY = "day";
+    private static final String STR_SCALE_WEEK = "week";
+    private static final String STR_SCALE_MONTH = "month";
 
-    private static final DateTime expectedFrom = DATE_FORMATTER.parseDateTime(STR_DATE_FROM);
-    private static final DateTime expectedTo = DATE_FORMATTER.parseDateTime(STR_DATE_TO);
+
+    private final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
     private GraphService graphService;
 
@@ -33,45 +34,45 @@ public class GraphServiceTest extends BaseTest {
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_StartNull() {
-        graphService.fromParameters(null, STR_DATE_TO);
+        graphService.fromParameters(null, STR_DATE_TO, STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_StartEmpty() {
-        graphService.fromParameters(STR_EMPTY, STR_DATE_TO);
+        graphService.fromParameters("", STR_DATE_TO, STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_EndNull() {
-        graphService.fromParameters(STR_DATE_FROM, null);
+        graphService.fromParameters(STR_DATE_FROM, null, STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_EndEmpty() {
-        graphService.fromParameters(STR_DATE_FROM, STR_EMPTY);
+        graphService.fromParameters(STR_DATE_FROM, "", STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_InvalidString1() {
-        graphService.fromParameters(STR_DATE_FROM, STR_INVALID_DATE_1);
+        graphService.fromParameters(STR_DATE_FROM, STR_INVALID_DATE_1, STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_InvalidString2() {
-        graphService.fromParameters(STR_DATE_FROM, STR_INVALID_DATE_2);
+        graphService.fromParameters(STR_DATE_FROM, STR_INVALID_DATE_2, STR_SCALE_WEEK);
     }
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_EndAfterStart() {
-        graphService.fromParameters(STR_DATE_TO, STR_DATE_FROM);
+        graphService.fromParameters(STR_DATE_TO, STR_DATE_FROM, STR_SCALE_WEEK);
     }
 
     @Test
     public void testFromParameters() {
-        final DateTime expectedFrom = DATE_FORMATTER.parseDateTime(STR_DATE_FROM);
-        final DateTime expectedTo = DATE_FORMATTER.parseDateTime(STR_DATE_TO);
+        final DateTime expectedFrom = dateFormatter.parseDateTime(STR_DATE_FROM);
+        final DateTime expectedTo = dateFormatter.parseDateTime(STR_DATE_TO);
 
-        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO);
+        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO, STR_SCALE_WEEK);
 
         assertEquals(expectedFrom, graphConfig.getFrom());
         assertEquals(expectedTo, graphConfig.getTo());
@@ -79,17 +80,41 @@ public class GraphServiceTest extends BaseTest {
 
     @Test(expected = InvalidInputException.class)
     public void testFromParameters_From_TodayString() {
-        graphService.fromParameters(STR_TODAY, STR_DATE_TO);
+        graphService.fromParameters(STR_TODAY, STR_DATE_TO, STR_SCALE_WEEK);
     }
 
     @Test
     public void testFromParameters_To_TodayString() {
-        final DateTime expectedFrom = DATE_FORMATTER.parseDateTime(STR_DATE_FROM);
-        final DateTime expectedTo = DATE_FORMATTER.parseDateTime(DATE_FORMATTER.print(new DateTime()));
+        final DateTime expectedFrom = dateFormatter.parseDateTime(STR_DATE_FROM);
+        final DateTime expectedTo = dateFormatter.parseDateTime(dateFormatter.print(new DateTime()));
 
-        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_TODAY);
+        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_TODAY, STR_SCALE_WEEK);
 
         assertEquals(expectedFrom, graphConfig.getFrom());
         assertEquals(expectedTo, graphConfig.getTo());
+    }
+
+
+    @Test(expected = InvalidInputException.class)
+    public void testFromParameters_Scale_Invalid() throws Exception {
+        graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO, "invalid");
+    }
+
+    @Test
+    public void testFromParameters_Scale_Day() throws Exception {
+        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO, STR_SCALE_DAY);
+        assertEquals(GraphConfig.Scale.DAY, graphConfig.getScale());
+    }
+
+    @Test
+    public void testFromParameters_Scale_Week() throws Exception {
+        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO, STR_SCALE_WEEK);
+        assertEquals(GraphConfig.Scale.WEEK, graphConfig.getScale());
+    }
+
+    @Test
+    public void testFromParameters_Scale_Month() throws Exception {
+        GraphConfig graphConfig = graphService.fromParameters(STR_DATE_FROM, STR_DATE_TO, STR_SCALE_MONTH);
+        assertEquals(GraphConfig.Scale.MONTH, graphConfig.getScale());
     }
 }
