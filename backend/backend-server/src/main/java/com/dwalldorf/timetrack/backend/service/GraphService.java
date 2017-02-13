9c275ace9dd3f40a64c98a207ce4245e0ebabdb6
Daniel Walldorf <d.walldorf@me.com>
2017-02-13 21:20:26 +0100

    GraphService

---
+++ backend/backend-server/src/main/java/com/dwalldorf/timetrack/backend/service/GraphService.java
@@ -0,0 +1,47 @@
+package com.dwalldorf.timetrack.backend.service;
+
+import com.dwalldorf.timetrack.backend.exception.InvalidInputException;
+import com.dwalldorf.timetrack.backend.model.GraphConfig;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GraphService {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
+
+    public GraphConfig fromParameters(String fromStr, String toStr) throws InvalidInputException {
+        if (fromStr == null) {
+            throw new InvalidInputException("fromStr must not be null");
+        }
+        if (toStr == null) {
+            throw new InvalidInputException("toStr must not be null");
+        }
+
+        DateTime from;
+        DateTime to = null;
+
+        if (toStr.equals("today")) {
+            toStr = DATE_FORMATTER.print(new DateTime());
+        }
+        try {
+            from = DATE_FORMATTER.parseDateTime(fromStr);
+
+            if (to == null) {
+                to = DATE_FORMATTER.parseDateTime(toStr);
+            }
+        } catch (IllegalArgumentException e) {
+            throw new InvalidInputException(e.getMessage(), e);
+        }
+
+        if (!from.isBefore(to)) {
+            throw new InvalidInputException("'from' must be before 'to'");
+        }
+
+        return new GraphConfig()
+                .setFrom(from)
+                .setTo(to);
+    }
+}
