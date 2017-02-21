b82430316e04a2c3feaa2b33db9233685bcaf55b
Daniel Walldorf <d.walldorf@me.com>
2017-02-21 22:04:42 +0100

    remove user settings

--- frontend/app/user/model/user.ts
+++ frontend/app/user/model/user.ts
@@ -1,5 +1,3 @@
-import {UserSettings} from "./user.settings";
-
 export class User {
 
     public id: string;
@@ -7,6 +5,5 @@ export class User {
     public email: string;
     public password: string;
     public registered: number;
-    public userSettings: UserSettings;
 
 }
