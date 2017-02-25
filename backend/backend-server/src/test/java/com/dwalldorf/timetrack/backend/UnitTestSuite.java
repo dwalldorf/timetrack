a650fd1862bde170cad5d58d82e3f3ebe959fdd9
Daniel Walldorf <d.walldorf@me.com>
2017-02-25 12:51:13 +0100

    unit test GraphDateTimeFormatterConfig

--- backend/backend-server/src/test/java/com/dwalldorf/timetrack/backend/UnitTestSuite.java
+++ backend/backend-server/src/test/java/com/dwalldorf/timetrack/backend/UnitTestSuite.java
@@ -1,6 +1,7 @@
 package com.dwalldorf.timetrack.backend;
 
 import com.dwalldorf.timetrack.backend.annotation.RequireLoginTest;
+import com.dwalldorf.timetrack.backend.config.GraphDateTimeFormatterConfigTest;
 import com.dwalldorf.timetrack.backend.rest.controller.UserControllerTest;
 import com.dwalldorf.timetrack.backend.service.CsvImportServiceTest;
 import com.dwalldorf.timetrack.backend.service.GraphServiceTest;
@@ -12,6 +13,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         RequireLoginTest.class,
+
+        GraphDateTimeFormatterConfigTest.class,
 
         UserControllerTest.class,
 
