2f9dc7ad86a6e1988b1404316ef7cd29b24829de
Daniel Walldorf <d.walldorf@me.com>
2017-03-14 20:55:28 +0100

    wip

--- docker/containers/grafana/config/grafana-conf.cmd
+++ docker/containers/grafana/config/grafana-conf.cmd
@@ -1,4 +1,5 @@
-curl -v POST http://localhost:3000/api/datasources -d @/etc/grafana/scripts/datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
+curl -v POST http://localhost:3000/api/datasources -d @/etc/grafana/scripts/influxdb.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
+curl -v POST http://localhost:3000/api/datasources -d @/etc/grafana/scripts/prometheus.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 
 curl -v POST http://localhost:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboards/users.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 curl -v POST http://localhost:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboards/performance.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
