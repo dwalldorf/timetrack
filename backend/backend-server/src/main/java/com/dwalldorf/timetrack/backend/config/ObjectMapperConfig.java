05d95f885b261e916b6eada0717cdac6cb2fa75d
Daniel Walldorf <d.walldorf@me.com>
2017-03-22 18:41:47 +0100

    use spring-boot-dependencies versions where possible

--- backend/backend-server/src/main/java/com/dwalldorf/timetrack/backend/config/ObjectMapperConfig.java
+++ backend/backend-server/src/main/java/com/dwalldorf/timetrack/backend/config/ObjectMapperConfig.java
@@ -1,14 +1,22 @@
 package com.dwalldorf.timetrack.backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObjectMapperConfig {
 
+    private ObjectMapper mapper;
+
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        if (mapper == null) {
+            this.mapper = new ObjectMapper();
+            this.mapper.registerModule(new JodaModule());
+        }
+
+        return mapper;
     }
 }
